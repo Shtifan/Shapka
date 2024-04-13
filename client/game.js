b18241e72f32298d13bd74771a9e44b9e1b08948
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-04-13 13:57:21 +0300

    fixed css

--- client/game.js
+++ client/game.js
@@ -2,16 +2,17 @@ let timeleft = 60;
 
 function startTimer() {
     let timer = setInterval(function () {
-        document.getElementById("timer").style.fontSize = "80px";
         timeleft--;
         document.getElementById("timer").textContent = `0:${timeleft < 10 ? "0" : ""}${timeleft}`;
         if (timeleft == 0) {
-            document.getElementById("timer").style.fontSize = "60px";
             document.getElementById("timer").innerHTML = "Time is up";
             timeleft = 60;
             clearInterval(timer);
         }
     }, 1000);
+
+    // Set initial font size
+    document.getElementById("timer").style.fontSize = "3rem"; // Adjust the font size here
 }
 
 document.getElementById("hatButton").addEventListener("click", function () {
